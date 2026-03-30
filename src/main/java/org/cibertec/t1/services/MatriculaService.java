c943a154e4460afca6ad16423b33153e37049c8d
frank <jeremioc6@gmail.com>
2026-03-29 23:28:56 -0500

    FASE GREEN : TEST FECHA MATRICULA

--- src/main/java/org/cibertec/t1/services/MatriculaService.java
+++ src/main/java/org/cibertec/t1/services/MatriculaService.java
@@ -1,5 +1,7 @@
 package org.cibertec.t1.services;
 
+import java.time.LocalDate;
+
 public class MatriculaService {
     public String registrar(String codMatricula, String alumno, String codCurso, int creditos, String fecha) {
         if (!codMatricula.matches("^[A-Z]{2}\\d{4}$")) {
@@ -14,6 +16,10 @@ public class MatriculaService {
         }
         if (creditos < 1 || creditos > 6) {
             return "La cantidad de créditos debe estar entre 1 y 6";
+        }
+        LocalDate fechaMatricula = LocalDate.parse(fecha);
+        if (fechaMatricula.isAfter(LocalDate.now())) {
+            return "Ingrese una fecha de matrícula válida";
         }
         return "La matrícula ha sido registrada correctamente";
     }
