f11c9648bf397f4356fca40a1802dfbe8f2de6d6
frank <jeremioc6@gmail.com>
2026-03-29 23:20:57 -0500

    FASE RED : TEST CODIGO CURSO

--- src/test/java/org/cibertec/t1/services/MatriculaServiceTest.java
+++ src/test/java/org/cibertec/t1/services/MatriculaServiceTest.java
@@ -18,4 +18,11 @@ class MatriculaServiceTest {
         String resultado = service.registrar("IN1234", "An1", "ABC12", 3, "2026-03-29");
         assertEquals("El nombre del estudiante debe tener al menos cuatro caracteres alfabéticos", resultado);
     }
+    @Test
+    public void testCodigoCursoInvalido() {
+
+        String resultado = service.registrar("IN1234", "Juan Perez", "ABC1", 3, "2026-03-29");
+        assertEquals("Ingrese un código de curso válido", resultado);
+    }
+
 }
