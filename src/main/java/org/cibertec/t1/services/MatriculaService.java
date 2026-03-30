53d7e00c20d2c18d8991a070994980674550c20a
frank <jeremioc6@gmail.com>
2026-03-29 23:01:16 -0500

    FASE GREEN : TEST CODIGO MATRICULA

--- src/main/java/org/cibertec/t1/services/MatriculaService.java
+++ src/main/java/org/cibertec/t1/services/MatriculaService.java
@@ -2,7 +2,9 @@ package org.cibertec.t1.services;
 
 public class MatriculaService {
     public String registrar(String codMatricula, String alumno, String codCurso, int creditos, String fecha) {
-
+        if (!codMatricula.matches("^[A-Z]{2}\\d{4}$")) {
+            return "Ingrese un código de matrícula válido";
+        }
         return "La matrícula ha sido registrada correctamente";
     }
 }
