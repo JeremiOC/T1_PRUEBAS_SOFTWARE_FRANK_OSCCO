7d96ec73be5d6555f7d7deaba30fded09e7d4624
frank <jeremioc6@gmail.com>
2026-03-29 23:21:21 -0500

    FASE GREEN : TEST CODIGO CURSO

--- src/main/java/org/cibertec/t1/services/MatriculaService.java
+++ src/main/java/org/cibertec/t1/services/MatriculaService.java
@@ -9,7 +9,9 @@ public class MatriculaService {
         if (!alumno.matches("^[a-zA-Z\\s]{4,}$")) {
             return "El nombre del estudiante debe tener al menos cuatro caracteres alfabéticos";
         }
-
+        if (!codCurso.matches("^[a-zA-Z0-9]{5}$")) {
+            return "Ingrese un código de curso válido";
+        }
         return "La matrícula ha sido registrada correctamente";
     }
 }
